291f27ba1336dc5b9b90d8918787815582f6ca0f
Matheus Marques <mgm3@icomp.ufam.edu.br>
2025-04-26 09:21:19 -0400

    feat: initial architecture of layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout-template/layout/layout.component';
+import { HomeComponent } from './layout-template/home/home.component';
+import { ContactComponent } from './layout-template/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      //   { path: '/', component: HomeComponent },
+      { path: 'contato', component: ContactComponent },
+    ],
+  },
+];
