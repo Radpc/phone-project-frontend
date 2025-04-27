daf821d6b4407bdf1d64feb15cc979bb12c2dd11
Matheus Marques <mgm3@icomp.ufam.edu.br>
2025-04-27 10:30:45 -0400

    feat: setup basic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout-template/layout/layout.component';
 import { HomeComponent } from './layout-template/home/home.component';
-import { ContactComponent } from './layout-template/contact/contact.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    children: [
-      //   { path: '/', component: HomeComponent },
-      { path: 'contato', component: ContactComponent },
-    ],
+    children: [{ path: '', component: HomeComponent }],
   },
 ];
--- src/app/layout-template/layout/layout.component.ts
+++ src/app/layout-template/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { SidebarComponent } from '../shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
