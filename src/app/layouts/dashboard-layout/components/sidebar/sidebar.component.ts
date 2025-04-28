7c130ba38710fa8deb664b35646da24e7abd0e6d
Matheus Marques <mgm3@icomp.ufam.edu.br>
2025-04-27 20:08:02 -0400

    feat: set active route style

--- src/app/layouts/dashboard-layout/components/sidebar/sidebar.component.ts
+++ src/app/layouts/dashboard-layout/components/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { ISidebarItem } from './sidebar-item.interface';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-
+import { ActivatedRoute, EventType, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
@@ -9,8 +9,19 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-  constructor(private activatedRoute: ActivatedRoute) {}
+export class SidebarComponent implements OnInit {
+  private route = inject(Router);
+  currentUrl = '';
 
   @Input() itemsList!: ISidebarItem[];
+
+  ngOnInit(): void {
+    this.route.events
+      .pipe(filter((event) => event.type === EventType.NavigationEnd))
+      .subscribe((ev: any) => {
+        this.currentUrl = ev.urlAfterRedirects;
+      });
+
+    this.currentUrl = this.route.url;
+  }
 }
